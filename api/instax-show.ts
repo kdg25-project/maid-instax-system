293d30e6cb3efcc6778a8159354df222fdab7e7f
takahashi <jin_25s1100444@nnn.ed.jp>
2025-11-10 15:23:13 +0900

    オーダー画面を表示

---
+++ api/instax-show.ts
@@ -0,0 +1,28 @@
+export type InstaxShowResponse = {
+    success: true;
+    message: string;
+    data: {
+        id: number,
+        user_id: string,
+        maid_id: string,
+        image_url: string
+    }
+} | {
+    success: false;
+    message: string;
+    details: string;
+}
+
+export function instaxShow(userId: string): Promise<InstaxShowResponse> {
+    const apiUrl = `https://api.kdgn.tech/api/instax/${userId}`;
+    return fetch(apiUrl)
+        .then((res) => res.json())
+        .catch((err) => {
+            console.warn(err);
+            return {
+                success: false,
+                message: "チェキ取得に失敗しました",
+                details: err.message
+            };
+        })
+}
