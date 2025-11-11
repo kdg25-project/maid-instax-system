1d82ac1de0fc9eb6f6a1c1ea7c09ac4f82829b73
hara <rikako25kdgn@gmail.com>
2025-11-11 18:51:27 +0900

    描画機能を追加し、カラーパレットと描画オプションを実装しました。また、パスワード認証機能を追加し、依存関係を更新しました。

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -5,27 +5,44 @@ import { useEffect, useState } from "react";
 
 import { useRouter } from "next/navigation";
 
+import Cookies from 'js-cookie';
+
 export default function Home() {
     const [id, setId] = useState(0);
     const [isError, setIsError] = useState(false);
     const router = useRouter();
+    const [isNeedPassword, setIsNeedPassword] = useState(() => !Cookies.get('maid_api_key'));
+    const [password, setPassword] = useState(() => Cookies.get('maid_api_key') ?? '');
 
     async function handleSubmit() {
-        const request = await fetch("https://api.kdgn.tech/api/instax/"+String(id))
+        if (isNeedPassword) {
+            Cookies.set('maid_api_key', password, { expires: 7, path: '/' });
+            setIsNeedPassword(false)
+        } else {
+            const request = await fetch("https://api.kdgn.tech/api/instax/"+String(id), {
+                headers: {
+                    "x-api-key": password,
+                }
+            })
         if (request.ok) {
             router.push("/edit/"+String(id));
         } else {
             setIsError(true)
         }
+        }
     }
 
-  return (
+    return (
     <div className="flex items-center justify-center flex-col p-10 gap-4">
-        <h1>入力してください</h1>
+        <h1>{isNeedPassword ? 'パスワード' : 'ID'}を入力してください</h1>
         <input
-        className="border p-2 rounded-2xl" type="number"
+        className="border p-2 rounded-2xl" type={isNeedPassword ? 'password' : 'number'}
         onChange={(e) => {
-          setId(Number(e.target.value));
+            if (isNeedPassword) {
+                setPassword(e.target.value)
+            } else {
+                setId(Number(e.target.value));
+            }
         }}
         />
         <button 
